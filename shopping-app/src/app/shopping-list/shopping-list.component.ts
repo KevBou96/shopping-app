369a08352394a32f948cdd5578e3333716774858
KevBou96 <kevenebou96@gmail.com>
2023-08-13 17:55:42 -0500

    feat: editing items component

--- shopping-app/src/app/shopping-list/shopping-list.component.ts
+++ shopping-app/src/app/shopping-list/shopping-list.component.ts
@@ -7,16 +7,21 @@ import { Ingredient } from '../shared/ingredient.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
   ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10)
   ];
 
   constructor() {
-
+    
   }
 
   ngOnInit(): void {
     
+  }
+
+  onNewIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
   }
 }
